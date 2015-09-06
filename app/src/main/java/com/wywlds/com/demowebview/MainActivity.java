15f56d89e1557f2b16999ae22305cd2995e6bb94
wyw <wywlds@gmail.com>
2015-09-06 10:46:30 +0800

    [update]从配置文件app/src/main/assets/target_url中取得url，然后在MainActivity中的WebView中打开。

--- app/src/main/java/com/wywlds/com/demowebview/MainActivity.java
+++ app/src/main/java/com/wywlds/com/demowebview/MainActivity.java
@@ -1,16 +1,29 @@
 package com.wywlds.com.demowebview;
 
+import android.app.Activity;
+import android.content.Context;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
 
 public class MainActivity extends ActionBarActivity {
 
     private WebView mWebView;
+    private Handler mHandler;
+    private Handler mUIHanlder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,19 +31,75 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         mWebView = (WebView) findViewById(R.id.webview);
-        mWebView.loadUrl("http://m.baidu.com");
 
         mWebView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                // TODO Auto-generated method stub
-                //返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
                 view.loadUrl(url);
                 return true;
             }
         });
 
     }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mUIHanlder = new Handler();
+        HandlerThread handlerThread = new HandlerThread("IOThread");
+        handlerThread.start();
+        mHandler = new Handler(handlerThread.getLooper());
+        mHandler.post(new Runnable() {
+
+            @Override
+            public void run() {
+                final String url = getStringFromFile(getApplicationContext(), "target_url", "utf-8");
+                Log.d("wyw", url);
+                mUIHanlder.post(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        mWebView.loadUrl(url);
+                    }
+                });
+            }
+        });
+    }
+
+    public static String getStringFromFile(Context context,
+                                           String fileName, String encoding) {
+        InputStream in = null;
+        BufferedReader reader = null;
+        try {
+            StringBuilder builder = new StringBuilder();
+            char[] buffer = new char[512];
+            in = context.getResources().getAssets().open(fileName);
+            reader = new BufferedReader(new InputStreamReader(in, encoding));
+            for (int length; (length = reader.read(buffer)) > 0;) {
+                builder.append(buffer, 0, length);
+            }
+            return builder.toString();
+        } catch (UnsupportedEncodingException e) {
+        } catch (IOException e) {
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+            }
+        }
+        return null;
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
